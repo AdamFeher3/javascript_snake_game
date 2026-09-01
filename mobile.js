00b66ed6e99013976d1423996f5aa93fad35c462
AdamFeher3 <feheradam717@gmail.com>
2026-09-01 19:01:36 +0200

    Added mobile controls

---
+++ mobile.js
@@ -0,0 +1,62 @@
+// MOBILE CONTROLS
+
+let hold = false;
+let smx = 0;
+let smy = 0;
+
+document.addEventListener("mousedown", (e) => {
+    
+    if ( game.state !== PLAY ) return;
+
+    smx = e.offsetX;
+    smy = e.offsetY;
+    hold = true;
+});
+
+document.addEventListener("mouseup", () => { hold = false; });
+
+document.addEventListener("mousemove", (e) => {
+
+    if ( game.state !== PLAY ) return;
+
+    if ( !hold ) return;
+
+    const mx = e.offsetX;
+    const my = e.offsetY;
+
+    resetPos(mx, my);
+
+    if ( mx > smx + 50 ) if ( smx - mx < 0 ) {
+
+        if ( snakeDir !== "L" && nextDir !== "L" ) nextDir = "R";
+        return;
+    }
+    if ( mx < smx - 50 ) if ( smx - mx > 0 ) {
+
+        if ( snakeDir !== "R" && nextDir !== "R" ) nextDir = "L";
+        return;
+    }
+    if ( my < smy - 50 ) if ( smy - my > 0 ) {
+
+        if ( snakeDir !== "D" && nextDir !== "D" ) nextDir = "U";
+        return;
+    }
+    if ( my > smy + 50 ) if ( smy - my < 0 ) {
+
+        if ( snakeDir !== "U" && nextDir !== "U" ) nextDir = "D";
+        return;
+    }
+});
+
+function resetPos(mx, my) {
+
+    if (
+        Math.abs(smx - mx) > 100 ||
+        Math.abs(smy - my) > 100
+    ) {
+        smx = mx;
+        smy = my;
+    }
+}
+
+document.addEventListener("contextmenu", (e) => { e.preventDefault(); });
